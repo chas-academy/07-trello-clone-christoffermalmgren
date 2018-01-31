07a3e784c44c8bc6d839e1668a069154b6dece7f
Christoffer Malmgren <christoffer.malmgren@chasacademy.se>
2018-01-31 15:32:18 +0100

    added a widget and had some small bugs

--- try.js
+++ try.js
@@ -1,19 +1,57 @@
+$.widget('superwidgets.borderify', {
+  options: {
+    color: "red",
+    width: "1px"
+  },
+
+  _create: function() {
+    this.element.css({'border' : this.options.width +' solid ' + this.options.color})
+  },
+
+  _setOption: function(key, value) {
+    if (key === "color") {
+      this.options.color = value;
+    } else if (key === "width") {
+      this.options.width = value;
+    }
+    this._super(key, value)
+  },
+
+  _setOptions: function(options) {
+    var that = this;
+    $.each(options, function(key, value) {
+      that._setOption(key, value)
+    })
+  }
+});
+
 $(document).ready(function() {
   $(".add-new").draggable({
     axis: "x"
   });
 
-  $(".board").sortable();
+  $(".board").sortable({
+    placeholder: "sortable-placeholder",
+    start: function(event, ui) {
+      ui.placeholder.height(ui.item.find('.list').height());
+    }
+  });
 
   $(".list-cards").sortable({
     //ändrar muspekaren plus att .list cards connectar med varandra.
     cursor: "move",
-    connectWith: ".list-cards"
+    connectWith: ".list-cards",
+    placeholder: "sortable-placeholder",
+    scroll: false,
+    start: function(event, ui) {
+      ui.placeholder.height(ui.item.outerHeight());
+      ui.placeholder.width(ui.item.outerWidth());
+    }
   });
 
   //add card
 
-  $(".new-card").submit(function(event) {
+   $("body").on("submit", ".new-card", function(event) {
     event.preventDefault(); //gör så sidan inte laddas om.
     var formData = $(event.target).serializeArray();
 
@@ -26,20 +64,13 @@ $(document).ready(function() {
             <a href="#" class="card-dialog">
             ${formData[0].value}
             </a>
-            <button class="button-delete">X</button>
+            <button class="button delete">X</button>
         </li>
     `;
 
     $(event.target)
       .closest(".add-new")
       .before(addNewCard);
-
-    //remove card
-    $(".button-delete").on("click", function() {
-      $(this)
-        .closest(".card")
-        .fadeOut("slow");
-    });
   });
 
   //add new list
@@ -55,7 +86,7 @@ $(document).ready(function() {
             <div class="list">
                 <div class="list-header">
                     ${formData[0].value}
-                    <button class="button-delete">X</button>
+                    <button class="button delete">X</button>
                 </div>
                 <ul class="list-cards">
 
@@ -74,11 +105,31 @@ $(document).ready(function() {
 
     //remove list. när du på Xet på listan så kickar functionen igång,
     //this refererar till button-delete och närmsta column class som tas bort.
-    $(".button-delete").on("click", function() {
-      $(this)
-        .closest(".column")
-        .toggle("slide");
-    });
+  });
+
+  //remove card
+  $("body").on("click", ".card > .button.delete", function(e) {
+    e.stopPropagation();
+
+    $(this)
+      .closest(".card")
+      .fadeOut("slow")
+      .remove();
+  });
+
+  $("body").on("click", ".list .button.delete", function(e) {
+    e.stopPropagation(); // Sluta bubbla eventet
+
+    $(this)
+      .closest(".column")
+      .toggle("slide", "slow", function() { 
+        this.remove() 
+      });
+  });
+
+  $("body").on("click", ".card-dialog", function() {
+    $(cardDetailsDialog).dialog("open");
+    $("#tabs-1 > p").borderify({"color": "pink", "width": "3px" });
   });
 
   //popup dialog vid knapp tryck på card.
@@ -96,17 +147,7 @@ $(document).ready(function() {
 
   $("#card-details-dialog #tabs").tabs();
 
-  $("body").on("click", ".card-dialog", function() {
-    $(cardDetailsDialog).dialog("open");
-  });
-
-
-    // i tabben due date dyker de upp ettt input field, trycker du där kommer en datepicker upp.
-    $("#datepicker").datepicker();
-       
-
-
-        
-        
+  // i tabben due date dyker de upp ettt input field, trycker du där kommer en datepicker upp.
+  $("#datepicker").datepicker();
     
 });
